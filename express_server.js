ae695f4cc127c02cdbc84f712d60ae2c749cb028
Myles Rademaker <mylesprademaker@gmail.com>
2017-01-17 21:45:55 +0000

    Created partials

--- express_server.js
+++ express_server.js
@@ -1,22 +1,34 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8080; // default port 8080
+const PORT = process.env.PORT || 8080;
+
+app.get('/urls', (request, response) =>{
+  let templateVars = {urls: urlDatabase};
+  response.render('urls_index', templateVars);
+});
+
+app.get('/urls/:id', (request, response) => {
+  let templateVars = { shortURL: request.params.id };
+  response.render("urls_show", templateVars);
+});
+
+app.set('view engine', 'ejs');
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
 
-app.get('/', (req, res) => {
-  res.end('Hello!');
+app.get('/', (request, response) => {
+  response.end('Hello!');
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get('/urls.json', (request, response) => {
+  response.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
+app.get('/hello', (request, response) => {
+  response.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
