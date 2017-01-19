9a7886c866c2e04ae4d7f8f2b86e0c79de6cf562
Myles Rademaker <mylesprademaker@gmail.com>
2017-01-19 01:03:37 +0000

    Added login and logout

--- express_server.js
+++ express_server.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+
+app.post('/login', (request, response) => {
+  response.cookie('username', request.body.username);
+  response.redirect('http://localhost:8080/urls');
+});
+
+app.post('/logout', (request, response) => {
+  response.clearCookie('username')
+  response.redirect('http://localhost:8080/');
+});
 
 app.get('/urls/new', (request, response) => {
+  let templateVars = { username: request.cookies['username']};
   response.render('urls_new');
 });
 
@@ -44,13 +57,13 @@ app.get('/u/:shortURL', (request, response) => {
 });
 
 app.get('/urls/:id', (request, response) => {
-  let templateVars = { shortURL: request.params.id };
-  templateVars.LongURL = urlDatabase[request.params.id] || 'Not in database';
+  let templateVars = { username: request.cookies['username'], shortURL: request.params.id };
+  templateVars.longURL = urlDatabase[request.params.id] || 'Not in database';
   response.render("urls_show", templateVars);
 });
 
 app.get('/urls', (request, response) =>{
-  let templateVars = {urls: urlDatabase};
+  let templateVars = {username: request.cookies['username'], urls: urlDatabase};
   response.render('urls_index', templateVars);
 });
 
@@ -66,13 +79,6 @@ app.get('/', (request, response) => {
   response.end('Hello and Welcome to TINY URL \n The Place For All Your Shortening Needs');
 });
 
-app.get('/urls.json', (request, response) => {
-  response.json(urlDatabase);
-});
-
-app.get('/hello', (request, response) => {
-  response.end('<html><body>Hello <b>World</b></body></html>\n');
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
